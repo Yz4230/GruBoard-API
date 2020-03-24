55feb8608793c158b8a6a57fa7cc7989ebc7cf04
Yz4230 <yuzupon1480@yahoo.co.jp>
2020-03-24 09:28:05 +0900

    Update CORS settings.

--- app/settings.py
+++ app/settings.py
@@ -44,8 +44,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    "https://gru-board.web.app",
+    "https://gru-board.firebaseapp.com",
+    "http://localhost:8080"
+]
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'app.urls'
